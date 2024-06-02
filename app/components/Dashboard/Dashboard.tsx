2d2d8a1e91b2e87adbbff21455f38e9c1b4eeb50
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-06-02 23:48:07 +0400

    dashbord changes: charts and expanding the information

--- app/components/Dashboard/Dashboard.tsx
+++ app/components/Dashboard/Dashboard.tsx
@@ -20,13 +20,14 @@ import {
   TabsTrigger,
 } from "@/components/ui/tabs"
 
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, BarChart, Bar, PieChart, Pie, Cell } from 'recharts';
+
 import {
   ChevronLeft,
   ChevronRight,
   Copy,
   File,
   Home,
-  LineChart,
   ListFilter,
   MoreVertical,
   Package,
@@ -81,27 +82,46 @@ import { Skeleton } from "@/components/ui/skeleton";
 export type TransactionStat = {
   monthly_sum: number
   monthly_change: number  
-  weekly_sum: number
-  weekly_change: number
   transactions_count: number
-  transactions_count_change: number
   top_category_current_month: string
-  top_category_previous_month: string
+  transactions_by_category: { category: string, total_sum: number }[]
+  total_monthly_sum: number
+  monthly_transactions_details: { transaction_date: string, converted_amount: number }[]
+  monthly_sum_comparison: { current_month_sum: number, previous_month_sum: number, absolute_change: number, percentage_change: number }
+  upcoming_recurring_transactions: { next_charge_date: string, amount: number, description: string }[]
+  total_upcoming_transactions_sum: number
+  transactions_by_month: { month: string, month_sum: number }[]
 }
 
 function processApiData(apiResponse: any): TransactionStat {
   return {
-    monthly_sum: apiResponse.monthly_sum,
-    monthly_change: apiResponse.monthly_change,
-    weekly_sum: apiResponse.weekly_sum,
-    weekly_change: apiResponse.weekly_change,
-    transactions_count: apiResponse.transactions_count,
-    transactions_count_change: apiResponse.transactions_count_change,
-    top_category_current_month: apiResponse.top_category_current_month ? apiResponse.top_category_current_month.category : 'None',
-    top_category_previous_month: apiResponse.top_category_previous_month ? apiResponse.top_category_previous_month.category : 'None',
+    monthly_sum: apiResponse.total_monthly_sum,
+    monthly_change: apiResponse.monthly_sum_comparison.percentage_change,
+    transactions_count: apiResponse.monthly_transactions_details.length,
+    top_category_current_month: apiResponse.top_category_current_month.category,
+    transactions_by_category: apiResponse.transactions_by_category,
+    total_monthly_sum: apiResponse.total_monthly_sum,
+    monthly_transactions_details: apiResponse.monthly_transactions_details.map((detail: any) => ({
+      transaction_date: detail.transaction_date,
+      converted_amount: detail.total_sum
+    })),
+    monthly_sum_comparison: apiResponse.monthly_sum_comparison,
+    upcoming_recurring_transactions: apiResponse.upcoming_recurring_transactions,
+    total_upcoming_transactions_sum: apiResponse.total_upcoming_transactions_sum,
+    transactions_by_month: apiResponse.transactions_by_month
   };
 }
 
+
+function formatDate(dateString: string): string {
+  const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long' };
+  return new Intl.DateTimeFormat('en-US', options).format(new Date(dateString));
+}
+
+function formatDateWithDay(dateString: string): string {
+  const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Intl.DateTimeFormat('en-US', options).format(new Date(dateString));
+}
 
 export function Dashboard() {
 
@@ -125,99 +145,108 @@ export function Dashboard() {
   return (
     <div className="flex min-h-screen w-full flex-col">
     <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-2">
       <Card x-chunk="dashboard-01-chunk-0">
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              Monthly
-            </CardTitle>
-            <DollarSign className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            {loading ? (
-              <>
-                <Skeleton className="h-6 w-full rounded-md" />
-                <Skeleton className="h-4 w-3/4 mt-2 rounded-md" />
-              </>
-            ) : (
-              <>
-                <div className="text-2xl font-bold">
-                  {`$${data?.monthly_sum ? data.monthly_sum.toFixed(2) : '0.00'}`}
-                </div>
-                <p className="text-xs text-muted-foreground">
-                  {`${data?.monthly_change}% change MoM`}
-                </p>
-              </>
-            )}
-          </CardContent>
-        </Card>
-        <Card x-chunk="dashboard-01-chunk-1">
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              Weekly
-            </CardTitle>
-            <DollarSign className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            {loading ? (
-              <>
-                <Skeleton className="h-6 w-full rounded-md" />
-                <Skeleton className="h-4 w-3/4 mt-2 rounded-md" />
-              </>
-            ) : (
-              <>
-                <div className="text-2xl font-bold">
-                  {`$${data?.weekly_sum ? data.weekly_sum.toFixed(2) : '0.00'}`}
-                </div>
-                <p className="text-xs text-muted-foreground">
-                  {`${data?.weekly_change}% change WoW`}
-                </p>
-              </>
-            )}
-          </CardContent>
-        </Card>
-        <Card x-chunk="dashboard-01-chunk-2">
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Transactions</CardTitle>
-            <CreditCard className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            {loading ? (
-              <>
-                <Skeleton className="h-6 w-full rounded-md" />
-                <Skeleton className="h-4 w-3/4 mt-2 rounded-md" />
-              </>
-            ) : (
-              <>
-                <div className="text-2xl font-bold">{data?.transactions_count}</div>
-                <p className="text-xs text-muted-foreground">
-                  {`${data?.transactions_count_change}% change MoM`}
-                </p>
-              </>
-            )}
-          </CardContent>
-        </Card>
-        <Card x-chunk="dashboard-01-chunk-3">
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Top Category</CardTitle>
-            <Activity className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            {loading ? (
-              <>
-                <Skeleton className="h-6 w-full rounded-md" />
-                <Skeleton className="h-4 w-3/4 mt-2 rounded-md" />
-              </>
-            ) : (
-              <>
-                <div className="text-2xl font-bold">{data?.top_category_current_month}</div>
-                <p className="text-xs text-muted-foreground">
-                  Last month «{data?.top_category_previous_month}»
-                </p>
-              </>
-            )}
-          </CardContent>
-        </Card>
+        <CardHeader className="flex flex-col items-start space-y-2">
+          <CardTitle className="font-semibold">
+            Spend this month
+          </CardTitle>
+          <CardDescription className="text-sm text-muted-foreground">
+            You spent {data?.monthly_sum ? data.monthly_sum.toFixed(2) : '0.00'} this month.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ResponsiveContainer width="100%" height={350}>
+            <LineChart
+              data={data?.monthly_transactions_details.map((detail) => ({ name: formatDateWithDay(detail.transaction_date), spending: detail.converted_amount }))}
+              margin={{ top: 5, right: 20, left: -30, bottom: 5 }}
+            >
+              <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+              <XAxis dataKey="name" stroke="rgba(0, 0, 0, 0.5)" tick={{ fontSize: '0.7rem' }} tickLine={false} />
+              <YAxis stroke="rgba(0, 0, 0, 0.5)" tick={{ fontSize: '0.7rem' }} tickLine={false} tickFormatter={(value) => value !== 0 ? value : ''} />
+              <Tooltip />
+              <Line type="monotone" dataKey="spending" stroke="#0F172A" dot={true} />
+            </LineChart>
+          </ResponsiveContainer>
+        </CardContent>
+      </Card>
+            <Card x-chunk="dashboard-01-chunk-1">
+            <CardHeader className="flex flex-col items-start space-y-2">
+                <CardTitle className="font-semibold">
+                  Spend by month
+                </CardTitle>
+                <CardDescription className="text-sm text-muted-foreground">
+  This month you already spent on {data?.monthly_sum_comparison?.absolute_change ? `${Math.abs(data.monthly_sum_comparison.absolute_change)} more` : 'the same amount'} than in last month.
+      </CardDescription>
+              </CardHeader>
+        <CardContent>
+          <ResponsiveContainer width="100%" height={350}>
+          <BarChart
+          data={data?.transactions_by_month.map((month) => ({ name: formatDate(month.month), spending: month.month_sum }))}
+          margin={{ top: 5, right: 20, left: -15, bottom: 5 }}
+        >
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" stroke="rgba(0, 0, 0, 0.5)" tick={{ fontSize: '0.7rem' }} tickLine={false} />
+          <YAxis stroke="rgba(0, 0, 0, 0.5)" tick={{ fontSize: '0.7rem' }} tickLine={false} tickFormatter={(value) => value !== 0 ? value : ''} />
+          <Tooltip />
+          <Bar dataKey="spending" fill="#0F172A" />
+        </BarChart>
+          </ResponsiveContainer>
+        </CardContent>
+      </Card>
+      <Card x-chunk="dashboard-01-chunk-2">
+      <CardHeader className="flex flex-col items-start space-y-2">
+                <CardTitle className="font-semibold">
+                  Spend by category this month
+                </CardTitle>
+                <CardDescription className="text-sm text-muted-foreground">
+                  Your highest category in this month is {data?.top_category_current_month}.
+                </CardDescription>
+              </CardHeader>
+  <CardContent>
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart
+        layout="vertical"
+        data={data?.transactions_by_category.map((category) => ({ name: category.category, value: category.total_sum }))}
+        margin={{ top: 5, right: 20, left: 15, bottom: 5 }}
+      >
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis type="number" stroke="rgba(0, 0, 0, 0.5)" tick={{ fontSize: '0.7rem' }} tickLine={false} />
+        <YAxis type="category" dataKey="name" stroke="rgba(0, 0, 0, 0.5)" tick={{ fontSize: '0.7rem' }} tickLine={false} />
+        <Tooltip />
+        <Bar dataKey="value" fill="#0F172A" />
+      </BarChart>
+    </ResponsiveContainer>
+  </CardContent>
+</Card>
+<Card x-chunk="dashboard-01-chunk-3">
+<CardHeader className="flex flex-col items-start space-y-2">
+                <CardTitle className="font-semibold">
+                  Upcoming transactions
+                </CardTitle>
+                <CardDescription className="text-sm text-muted-foreground">
+                  The sum of your upcoming transactions this month is ${data?.total_upcoming_transactions_sum.toFixed(2)}.
+                </CardDescription>
+              </CardHeader>
+  <CardContent className="grid gap-2">
+    {data?.upcoming_recurring_transactions.map((transaction, index) => (
+      <div key={index}>
+        <div className="flex items-center gap-4">
+          <div className="grid gap-1">
+            <p className="text-sm font-medium leading-none">
+              {transaction.description}
+            </p>
+            <p className="text-xs text-gray-500 opacity-50">
+              Charge date: {transaction.next_charge_date}
+            </p>
+          </div>
+          <div className="ml-auto font-medium">{`$${transaction.amount.toFixed(2)}`}</div>
+        </div>
+        {index < data?.upcoming_recurring_transactions.length - 1 && <hr className="my-2" />}
+      </div>
+    ))}
+  </CardContent>
+</Card>
         </div>
         <div className="grid gap-4 md:gap-8 lg:grid-cols-1 xl:grid-cols-1">
           <Card
@@ -225,7 +254,7 @@ export function Dashboard() {
           >
           <CardHeader className="flex flex-row items-center justify-between">
             <div>
-              <CardTitle className="mb-2">Transactions</CardTitle>
+              <CardTitle className="mb-2">All Transactions</CardTitle>
               <CardDescription>
                 Recent transactions from your wallet.
               </CardDescription>
